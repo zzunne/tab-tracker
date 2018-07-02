ac3b460fc594ad96fb1fe1e55c2bb4e56f52a62f
zzunne <kjoon9236@gmail.com>
2018-07-02 22:29:31 +0900

    add login

---
+++ client/src/components/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="login">
+    <div v-show="!isLogged">
+        <form>
+            <div class="form-group">
+                <label for="id">ID</label>
+                <input class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter ID" v-model="id">
+                <small id="emailHelp" class="form-text text-muted">개인정보 책임지지않는다.</small>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
+                <small id="emailHelp" class="form-text text-muted">암호화 하지않는다.</small>
+            </div>
+            <button class="btn btn-primary" @click="submit">Submit</button>
+        </form>
+    </div>
+    <div v-show="isLogged">
+        이미 로그인되어있어용.
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      id: '',
+      password: ''
+    }
+  },
+  computed: {
+    isLogged () {
+        return this.$store.getters.isLogged
+    }
+  },
+  methods: {
+      logIn(token){
+          this.$store.commit('logIn', {
+              id: this.id,
+              token: token
+          })
+      },
+      submit: function(){
+          var json = {
+              id: this.id,
+              password: this.password
+          }
+          this.$http.post(this.$config.targetURL+'/user/login', json)
+          .then((result)=>{
+              if(result.data.status == 'success'){
+                  console.log('success')
+                  this.logIn(result.data.token)
+                    this.$notice({
+                        type: 'success',
+                        text: '무사히 로그인 성공!'
+                    })
+
+              }
+              else {
+                console.log('error')
+                    this.$notice({
+                        type: 'alert',
+                        text: '로그인 정보가 올바르지 않습니다'
+                    })
+              }
+            })
+          .catch((error)=>{
+            console.log('server success')
+            this.$notice({
+                type: 'alert',
+                text: '서버에 오류가 있습니다.'
+            })
+
+          })
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
